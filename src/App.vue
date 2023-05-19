3e68c1808d21200912ac6bac8b0e33e205a376b4
felipedossantosoliveira <felipedossantosads@gmail.com>
2023-05-19 14:31:31 -0300

    feat: add app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<header class="flex items-center justify-between m-10">
+  <div class="text-3xl font-bold">
+    <RouterLink to="/">Vue 3 Charts Examples</RouterLink>
+  </div>
+</header>
+<div class="m-10">
+  <RouterView />
+</div>
+</template>
---
+++ src/views/ExamplesView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import LineChartExample from "../components/LineChartExample.vue";
+import MultiLineChartExample from "../components/MultiLineChartExample.vue";
+import BarsChartExample from "../components/BarsChartExample.vue";
+import { markRaw, reactive, ref } from "vue";
+
+const LineChartExampleS = markRaw(LineChartExample)
+const MultiLineChartExampleS = markRaw(MultiLineChartExample)
+const BarsChartExampleS = markRaw(BarsChartExample)
+
+const component = ref(LineChartExampleS)
+
+</script>
+<template>
+  <div class="flex">
+    <div class="grid h-20">
+      <div class="text-left p-4">
+        <h1 class="text-xl font-semibold">Examples</h1>
+      </div>
+      <button @click="component = LineChartExampleS" class="text-left ml-4 p-2 border rounded-md mb-2">
+        LineChart
+      </button>
+      <button @click="component = MultiLineChartExampleS" class="text-left ml-4 p-2 border rounded-md mb-2">
+        MultiLineChart
+      </button>
+      <button @click="component = BarsChartExampleS" class="text-left ml-4 p-2 border rounded-md mb-2">
+        BarChart
+      </button>
+    </div>
+    <div class="w-full m-5">
+      <component :is="component" />
+    </div>
+  </div>
+  <div></div>
+</template>
